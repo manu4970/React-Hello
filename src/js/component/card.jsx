70e31a4b1af4cb4ecf7b4cc5dfee8261d02333ac
Manuel Perez <manuelperez497078@gmail.com>
2023-03-31 18:52:01 -0300

    se instala eslint

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -25,7 +25,7 @@ const Card = () => {
         <h5 className="card-title text-center">Card title</h5>
         <p className="card-text text-center">
           Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          bulk of the cards content.
         </p>
         <a
           href="#"
